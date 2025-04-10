3d2ff939b09a2cbfd2c3ea5ab54fcd7256f5bd6f
Aryan Goel <aryangoel27@gmail.com>
2025-04-09 19:05:49 -0700

    Added app skeleton

---
+++ app/photoSets/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PhotoSet = () => {
+  return (
+    <View>
+      <Text>PhotoSet</Text>
+    </View>
+  );
+};
+
+export default PhotoSet;
