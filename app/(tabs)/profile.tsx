ec366e9ff358f760ea4cdcc6f1e3c635107b2c0c
Aryan Goel <aryangoel27@gmail.com>
2025-04-16 20:39:27 -0700

    removed ImageChooser component

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,32 @@
-import ImageChooser from "@/components/ImageChooser";
+import { useState } from "react";
+import { Button, Image, View, Text, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 const profile = () => {
-  return <ImageChooser />;
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsEditing: true,
+      aspect: [4, 5],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} className="w-48 h-48 mt-4" />}
+    </View>
+  );
 };
 
 export default profile;
