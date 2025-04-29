0914be7ffd8a6404236c97cef752f04bf8b57db9
Stuart Arief <stuartarief@ucr-secure-21-10-13-135-204.wnet.ucr.edu>
2025-04-29 14:44:08 -0700

    Added Icons to each Tab, added all necessary Tabs

---
+++ app/(tabs)/chat.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const chat = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Chat</Text>
+    </View>
+  );
+};
+
+export default chat;
---
+++ app/(tabs)/post.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const post = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Post</Text>
+    </View>
+  );
+};
+
+export default post;
