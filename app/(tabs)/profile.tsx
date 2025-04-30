fe04839e0fbdee8b853cd84958f900419f24b68a
SpaceMonkey2474 <kaseklau@gmail.com>
2025-04-30 00:25:28 -0700

    Added functionatlity to add and remove images to profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,16 @@
 import ProfileTop from "@/components/profile/ProfileTop";
+// Allow for upoading files into profile
+import PhotoSetUploader from "@/components/profile/PhotoSetUploader";
+import { ScrollView, View } from "react-native";
 
 const profile = () => {
   return (
-    <>
+    <ScrollView>
       <ProfileTop />
-    </>
+      <View className="mt-6 px-4">
+        <PhotoSetUploader />
+      </View>
+    </ScrollView>
   );
 };
 
