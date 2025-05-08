d1b6995a330d8d79d0e30904030bbd92afaa8484
Stuart Arief <stuartarief@ucr-secure-21-10-13-133-175.wnet.ucr.edu>
2025-05-07 19:34:05 -0700

    Added react import to layout.tsx

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
+import React from 'react';
 
 export default function RootLayout() {
   return (
